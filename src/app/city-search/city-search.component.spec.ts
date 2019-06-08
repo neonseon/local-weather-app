f4cc5c3f4bb117f29e08978d8aad8c8f4d636b36
Seon Ricks <skcirs@gmail.com>
2019-06-08 11:40:02 -0700

    Passing unit tests

--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CitySearchComponent } from './city-search.component';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CitySearchComponent', () => {
   let component: CitySearchComponent;
@@ -8,6 +10,17 @@ describe('CitySearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
       declarations: [ CitySearchComponent ]
     })
     .compileComponents();
--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { WeatherService } from '../weather/weather.service';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
@@ -14,7 +13,7 @@ export class CitySearchComponent implements OnInit {
 
   search = new FormControl('', [Validators.minLength(3)])
 
-  constructor(private weatherService: WeatherService) { }
+  constructor() { }
 
   ngOnInit() {
     this.search.valueChanges.pipe(debounceTime(1000)).subscribe((searchValue : string) => {
